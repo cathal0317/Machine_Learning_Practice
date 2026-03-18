974e6c13f1e69ec5f342fc60a918a1908f87415b
Cathal0317 <hlzh0610@gmail.com>
2026-03-18 19:06:06 +0000

    training loop and testing added

--- Programming_AI/VAE.py
+++ Programming_AI/VAE.py
@@ -81,4 +81,92 @@ def loss_func(x, recon_x, mu, log_var):
 
 model = VAE().to(device)
 
-optimiser = torch.optim.Adam(mdoel.parameter(), lr = learning_rate)
+optimiser = torch.optim.Adam(model.parameters(), lr = lr)
+
+# Train
+import time
+import copy
+def train_model(model, dataloader, criterion, optimiser, num_epochs=10):
+
+    since = time.time()
+
+    train_loss_history = []
+    val_loss_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_val_loss = 100000000
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            
+            else:
+                model.eval()
+
+            running_loss = 0.0
+
+            for inputs, labels in dataloader[phase]:
+                inputs = inputs.to(device)
+
+                optimiser.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+
+                    outputs, mu, log_var = model(inputs)
+                    loss = criterion(inputs, outputs, mu, log_var)  # calculate a loss
+
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()                             # perform back-propagation from the loss
+                        optimiser.step()                             # perform gradient descent with given optimiser
+
+                # statistics
+                running_loss += loss.item()
+
+            epoch_loss = running_loss / len(dataloader[phase].dataset)
+
+            print('{} Loss: {:.4f}'.format(phase, epoch_loss))
+            
+            # deep copy the model
+            if phase == 'train':
+                train_loss_history.append(epoch_loss)
+
+            if phase == 'val':
+                val_loss_history.append(epoch_loss)
+
+            if phase == 'val' and epoch_loss < best_val_loss:
+                best_val_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+            
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Loss: {:4f}'.format(best_val_loss))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, train_loss_history, val_loss_history
+best_model, train_loss_history, val_loss_history = train_model(model, dataloader, loss_func, optimiser, num_epochs=num_epochs)
+
+# Test
+with torch.no_grad():
+    running_loss = 0.0
+    for inputs, labels in dataloaders["test"]:
+        inputs = inputs.to(device)
+
+        outputs, mu, log_var = best_model(inputs)
+        test_loss = loss_func(inputs, outputs, mu, log_var)
+        
+        running_loss += test_loss.item()
+
+    test_loss = running_loss / len(dataloaders["test"].dataset)
+    print(test_loss)        
+out_img = torch.squeeze(outputs.cpu().data)
+print(out_img.size())
